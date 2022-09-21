aa93d15df9141d894ea37dfae8f1edefdc0f10e1
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2022-09-21 10:51:09 -0500

    initialization for project to work

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var corsOptions = {
   origin: "http://localhost:8081"
 };
 app.use(cors(corsOptions));
+app.options('*', cors())
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
@@ -22,7 +23,7 @@ app.get("/", (req, res) => {
 });
 require("./app/routes/tutorial.routes")(app);
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
